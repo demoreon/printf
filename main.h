d098ce7ffff43bdc1618a4225cff103fd10efa0f
demoreon <demoreon@gmail.com>
2023-04-30 15:47:49 +0100

    %d and %i added

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <limits.h>
 #define PRINT(a) _putchar(a)
 
 void _putchar(char c);
--- print.c
+++ print.c
@@ -1,5 +1,37 @@
 #include "main.h"
 
+int print_int(int num, int len)
+{
+	int div = 1;
+
+	if (num == 0)
+	{
+		PRINT(0);
+		len++;
+		return (len);
+	}
+	if (num == INT_MIN)
+	{
+		PRINT('-');
+		PRINT('2');
+		num = 147483648;
+	} else if (num < 0)
+	{
+		PRINT('-');
+		num = -num;
+	}
+	while (num / div >= 10) /* scale div to the dividend value */
+		div *= 10;
+	while (div > 0)
+	{
+		PRINT(num / div + 48);
+		num %= div;
+		div /= 10;
+		len++;
+	}
+	return (len);
+}
+
 /**
  * _printf - a variadic function that can print an unknown number of args
  * @format: The argument to parse
@@ -34,6 +66,10 @@ int _printf(const char *format, ...)
 				break;
 			case '%':
 				PRINT(*c);
+				break;
+			case 'd':
+			case 'i':
+				len = print_int(va_arg(args, int), len - 1);
 				break;
 			default:
 				PRINT(*(c - 1));
